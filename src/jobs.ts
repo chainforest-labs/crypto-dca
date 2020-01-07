7f4987eed27bf630138629281abde9f8fee57721
Satyajeet Pal <psatyajeet@gmail.com>
2020-01-06 17:20:07 -0800

    Add log when jobs starts

--- src/jobs.ts
+++ src/jobs.ts
@@ -1,4 +1,6 @@
 import { CronJob } from 'cron';
 import { buyCoins } from './buyCoins';
 
+console.log('Initializing jobs...');
+
 new CronJob('0 00 12 * * *', buyCoins, '', true, 'America/New_York');
