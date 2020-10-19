78086d27b574890acef2362fbd88e9b5594b8907
Satyajeet Pal <psatyajeet@gmail.com>
2020-10-19 09:05:59 -0700

    Reduce the MIN BALANCE because of Coinbase instant deposits

--- src/addFunds.ts
+++ src/addFunds.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import _ from 'lodash';
 import { authedClient, checkSufficientFunds, makeDeposit } from './coinbase';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -9,11 +10,13 @@ const BIWEEKLY_BUY = process.env.BIWEEKLY_BUY_AMOUNT
   ? parseFloat(process.env.BIWEEKLY_BUY_AMOUNT)
   : 0;
 
+const DAILY_BUY = _.round(BIWEEKLY_BUY / 14, 2);
+
 export async function addFunds() {
   const accounts = await authedClient.getAccounts();
-  if (!checkSufficientFunds(accounts, 'USD', BIWEEKLY_BUY / 2)) {
-    console.log(`Insufficient funds for upcoming week ($${BIWEEKLY_BUY / 2})!`);
-    const result = await makeDeposit(BIWEEKLY_BUY / 10); // it takes 5 days for BIWEEKLY_BUY / 2 to clear
+  if (!checkSufficientFunds(accounts, 'USD', DAILY_BUY)) {
+    console.log(`Insufficient funds for upcoming week ($${DAILY_BUY})!`);
+    const result = await makeDeposit(DAILY_BUY); // it takes 1 days for DAILY_BUY to clear
     console.log(
       `Initiated deposit of ${result.amount} with id ${result.id} paying out at ${result.payout_at}`,
     );
