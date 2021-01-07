1498baa98dc2f9ced184774bc7841da83fe3b6fe
Satyajeet Pal <psatyajeet@gmail.com>
2021-01-06 20:55:37 -0800

    Switch cron job to having delay

--- src/jobs.ts
+++ src/jobs.ts
@@ -1,9 +1,9 @@
+import { CronJob } from 'cron';
+
 import { addFunds } from './addFunds';
 import { buyCoins } from './buyCoins';
 
 console.log('Initializing jobs...');
 
-// new CronJob('0 50 11 * * *', addFunds, '', true, 'America/New_York');
-// new CronJob('0 00 12 * * *', buyCoins, '', true, 'America/New_York');
-
-addFunds().then(() => buyCoins());
+new CronJob('0 45 11 * * *', addFunds, '', true, 'America/New_York');
+new CronJob('0 00 12 * * *', buyCoins, '', true, 'America/New_York');
