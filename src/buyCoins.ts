fcbb6fa2b2398cab4b04dc6f201c27d8c5c22c50
Satyajeet Pal <psatyajeet@gmail.com>
2021-04-21 09:37:22 -0700

    Update crypto dca to be able to buy any asset

--- src/buyCoins.ts
+++ src/buyCoins.ts
@@ -12,6 +12,8 @@ if (process.env.NODE_ENV !== 'production') {
 const BIWEEKLY_BUY = process.env.BIWEEKLY_BUY_AMOUNT
   ? parseFloat(process.env.BIWEEKLY_BUY_AMOUNT)
   : 0;
+const COIN_TO_BUY = process.env.COIN_TO_BUY;
+
 const DAILY_BUY = _.round(BIWEEKLY_BUY / 14, 2);
 
 function sleep(ms: number) {
@@ -28,33 +30,37 @@ function checkFilled(fills: any, orderId: string) {
   return false;
 }
 
+async function buyCoin(coin, amount) {
+  const buyParams: MarketOrder = {
+    funds: amount.toString(), // USD
+    product_id: `${coin}-USD`,
+    type: 'market',
+    side: 'buy',
+    size: null,
+  };
+  console.log(`Buying...$${amount} ${coin}`);
+  const order = await authedClient.placeOrder(buyParams);
+
+  await sleep(5000);
+
+  const fills = await authedClient.getFills({
+    product_id: `${coin}-USD`,
+    order_id: order.id,
+  });
+
+  await wait(60 * 1000);
+  if (checkFilled(fills, order.id)) {
+    console.log(`Success! Order: ${order.id}`);
+  } else {
+    console.log(`Order ${order.id} did not fill`);
+  }
+}
+
 export async function buyCoins() {
   const accounts = await authedClient.getAccounts();
   if (!checkSufficientFunds(accounts, 'USD', DAILY_BUY)) {
     console.log(`Insufficient funds for daily buy ($${DAILY_BUY}) !`);
   } else {
-    const buyParams: MarketOrder = {
-      funds: DAILY_BUY.toString(), // USD
-      product_id: 'BTC-USD',
-      type: 'market',
-      side: 'buy',
-      size: null,
-    };
-    console.log(`Buying...$${DAILY_BUY} BTC`);
-    const order = await authedClient.placeOrder(buyParams);
-
-    await sleep(5000);
-
-    const fills = await authedClient.getFills({
-      product_id: 'BTC-USD',
-      order_id: order.id,
-    });
-
-    await wait(60 * 1000);
-    if (checkFilled(fills, order.id)) {
-      console.log(`Success! Order: ${order.id}`);
-    } else {
-      console.log(`Order ${order.id} did not fill`);
-    }
+    await buyCoin(COIN_TO_BUY, DAILY_BUY);
   }
 }
